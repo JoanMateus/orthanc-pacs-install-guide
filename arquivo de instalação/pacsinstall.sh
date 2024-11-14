28c92bf90d80ab6bc57b3cb925c06f0bef4d81b5
Joan Mateus <62283438+JoanMateus@users.noreply.github.com>
2024-11-14 09:22:42 -0300

    Add files via upload
    arquivo de instalação

---
+++ arquivo de instalação/pacsinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#instalação do orthanc
+
+apt update && apt upgrade -y
+
+apt install orthanc orthanc-gdcm orthanc-python orthanc-dicomweb orthanc-imagej orthanc-webviewer orthanc-wsi orthanc-mysql
+
+
+
